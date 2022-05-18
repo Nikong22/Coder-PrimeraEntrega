e60cd3ad4a4e306551f7a9bf63697c1309b73a35
Nicolás Cammarota <76768475+Nikong22@users.noreply.github.com>
2022-05-17 21:49:15 -0300

    Admin

--- api/productos.js
+++ api/productos.js
@@ -15,19 +15,31 @@ router.get("/productos/:id", (req, res) => {
   res.json(producto);
  });
 
-router.post("/productos", function (req, res) {
-  productos_controller.nuevoProducto(req);
-  res.send("Producto Añadido");
+ router.post("/productos", function (req, res) {
+  if (req.query.admin) {
+    productos_controller.nuevoProducto(req);
+    res.send("Producto Añadido");
+  } else {
+    res.send({ error: -1, descripcion: "ruta 'x' método 'y' no autorizada" })
+  }
 });
 
 router.put("/productos/:id", (req, res) => {
-  productos_controller.actualizarProducto(req);
-  res.send("Producto Actualizado");
+  if (req.query.admin) {
+    productos_controller.actualizarProducto(req);
+    res.send("Producto Actualizado");
+  } else {
+    res.send({ error: -1, descripcion: "ruta 'x' método 'y' no autorizada" })
+  }
 });
 
 router.delete("/productos/:id", (req, res) => {
-  productos_controller.borrarProducto(req);
-  res.send("Producto Borrado");
+  if (req.query.admin) {
+    productos_controller.borrarProducto(req);
+    res.send("Producto Borrado");
+  } else {
+    res.send({ error: -1, descripcion: "ruta 'x' método 'y' no autorizada" })
+  }
 });
 
 module.exports = router;
